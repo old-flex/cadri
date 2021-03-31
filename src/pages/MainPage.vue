eb6a33140ad2d04f397545f089e4e3048011c324
old-flex <kostyaniusgaming@gmail.com>
2021-03-31 14:29:04 +0700

    Обновил главную страницу

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,18 +1,77 @@
 <template>
-<q-layout>
-  <q-page-container>
-    <q-page>
-      <q-card>
-        <q-select filled v-model="currentSubdivision" :options="allSubdivisions"/>
-        <q-table
-          :columns="columns"
-          :data="filterEmployees"
-          row-key="name"
-          title="Сотрудники"
-        >
+<q-layout view="lHr lpR lFr">
+  <q-header elevated class="bg-primary text-white">
+    <q-toolbar>
+      <q-avatar>
+        <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg">
+      </q-avatar>
+      <q-toolbar-title>
+        Кадровый помощник
 
-        </q-table>
-      </q-card>
+        <q-btn style="margin-left: 40px" color="secondary" label="Табель" icon-right="expand_more">
+          <q-menu fit>
+            <q-list>
+              <q-item clickable v-close-popup>
+                <q-item-section>
+                  Такие табеля
+                </q-item-section>
+              </q-item>
+              <q-item clickable v-close-popup>
+                <q-item-section>
+                  Сякие табеля
+                </q-item-section>
+              </q-item>
+            </q-list>
+          </q-menu>
+        </q-btn>
+
+        <q-btn style="margin-left: 40px" color="secondary" label="График отпусков" icon-right="expand_more">
+          <q-menu fit>
+            <q-list>
+              <q-item clickable v-close-popup>
+                <q-item-section>
+                  Графики
+                </q-item-section>
+              </q-item>
+              <q-item clickable v-close-popup>
+                <q-item-section>
+                  Печать
+                </q-item-section>
+              </q-item>
+            </q-list>
+          </q-menu>
+        </q-btn>
+
+      </q-toolbar-title>
+      <q-btn flat round dense icon="logout" />
+    </q-toolbar>
+  </q-header>
+
+  <q-page-container>
+    <q-page class="q-pa-md">
+        <div class="flex flex-center column">
+          <div style="width: 80%;">
+            <q-select style="max-width: 210px; margin-left: auto"  filled v-model="currentSubdivision" :options="allSubdivisions"/>
+          </div>
+          <q-card style="width: 80%;">
+            <q-table
+              :columns="columns"
+              :data="filterEmployees"
+              row-key="name"
+              title="Сотрудники"
+            >
+              <template v-slot:top-right>
+                <q-input  dense debounce="300" v-model="filter" placeholder="Поиск">
+                  <template v-slot:append>
+                    <q-icon name="search" />
+                  </template>
+                </q-input>
+              </template>
+
+            </q-table>
+          </q-card>
+        </div>
+
     </q-page>
   </q-page-container>
 </q-layout>
@@ -26,12 +85,14 @@ export default {
       currentSubdivision: 'Бухгалтерия',
       allSubdivisions: ['Бухгалтерия', 'Учебный отдел', 'Отдел кадров'],
       isLoading: false,
+      filter: '',
       columns: [
         {name: 'lastname', required: true, label: 'Фамилия', align: 'left', field: 'lastname', sortable: true,},
         {name: 'firstname', required: true, label: 'Имя', align: 'left', field: 'firstname', sortable: true},
         {name: 'patronymic', required: true, label: 'Отчество', align: 'left', field: 'patronymic', sortable: true},
-        {name: 'phone', required: true, label: 'Телефон', align: 'left', field: 'phone', sortable: true},
+        {name: 'position', required: true, label: 'Должность', align: 'left', field: 'position', sortable: true},
         {name: 'subdivision', required: true, label: 'Подразделение', align: 'left', field: 'subdivision', sortable: true},
+        {name: 'phone', required: true, label: 'Телефон', align: 'left', field: 'phone', sortable: true},
       ],
       employees: [],
     }
@@ -63,6 +124,8 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style>
+th {
+  font-weight: 700;
+}
 </style>
