2c4802b88484d50955f0f314bdfdb5ceedba5246
old-flex <kostyaniusgaming@gmail.com>
2021-04-08 03:27:43 +0700

    Добавлена главная страница с таблицей и выбором подразделения

--- src/pages/EditReport.vue
+++ src/pages/EditReport.vue
@@ -3,23 +3,33 @@
     <q-page-container>
       <q-page>
         <div class="flex items-center">
-          <div class="q-mr-lg">
-            Имя фамилия
-          </div>
-          <q-table
-            dense
-            :data="data"
-            title="QDataTable with QPopupEdit"
-            :rows-per-page-options="[]"
-            row-key="name"
-            separator="cell"
-          >
-            <template #body="">
-              <q-td>
-                123
-              </q-td>
-            </template>
-          </q-table>
+          <q-markup-table separator="cell" flat bordered dense>
+            <tbody v-for="index1 in 10" :key="index1">
+            <tr>
+              <td v-if="index1 === 1" rowspan="2" class="text-center">Фамилия, инициалы, должность</td>
+              <td v-if="index1 === 1" rowspan="2" class="text-center">Табельный номер</td>
+              <td v-if="index1 === 1" colspan="31" class="text-center">Отметки о явках и неявках</td>
+            </tr>
+            <tr>
+              <td v-for="index in 31" :key="index">
+                {{ index }}
+              </td>
+            </tr>
+            <tr>
+              <td rowspan="2"  class="text-center">Витя Петров, <p>инженер</p></td>
+              <td rowspan="2"  class="text-center">1123</td>
+              <td class="text-center" v-for="index in 31" :key="index">
+                <q-select dense v-model="model[index]" :options="options"/>
+              </td>
+            </tr>
+            <tr>
+              <td class="text-center" v-for="index in 31" :key="index">
+                8
+              </td>
+            </tr>
+            </tbody>
+
+          </q-markup-table>
         </div>
       </q-page>
     </q-page-container>
@@ -27,125 +37,14 @@
 </template>
 
 <script>
-const columns = [
-  { name: 'desc', align: 'left', label: 'Dessert (100g serving)', field: 'name' },
-  { name: 'calories', align: 'center', label: 'Calories', field: 'calories' },
-  { name: 'fat', label: 'Fat (g)', field: 'fat' },
-  { name: 'carbs', label: 'Carbs (g)', field: 'carbs' },
-  { name: 'protein', label: 'Protein (g)', field: 'protein' },
-  { name: 'sodium', label: 'Sodium (mg)', field: 'sodium' },
-  { name: 'calcium', label: 'Calcium (%)', field: 'calcium' },
-  { name: 'iron', label: 'Iron (%)', field: 'iron' }
-]
-
-const data = [
-  {
-    name: 'Frozen Yogurt',
-    calories: 159,
-    fat: 6.0,
-    carbs: 24,
-    protein: 4.0,
-    sodium: 87,
-    calcium: '14%',
-    iron: '1%'
-  },
-  {
-    name: 'Ice cream sandwich',
-    calories: 237,
-    fat: 9.0,
-    carbs: 37,
-    protein: 4.3,
-    sodium: 129,
-    calcium: '8%',
-    iron: '1%'
-  },
-  {
-    name: 'Eclair',
-    calories: 262,
-    fat: 16.0,
-    carbs: 23,
-    protein: 6.0,
-    sodium: 337,
-    calcium: '6%',
-    iron: '7%'
-  },
-  {
-    name: 'Cupcake',
-    calories: 305,
-    fat: 3.7,
-    carbs: 67,
-    protein: 4.3,
-    sodium: 413,
-    calcium: '3%',
-    iron: '8%'
-  },
-  {
-    name: 'Gingerbread',
-    calories: 356,
-    fat: 16.0,
-    carbs: 49,
-    protein: 3.9,
-    sodium: 327,
-    calcium: '7%',
-    iron: '16%'
-  },
-  {
-    name: 'Jelly bean',
-    calories: 375,
-    fat: 0.0,
-    carbs: 94,
-    protein: 0.0,
-    sodium: 50,
-    calcium: '0%',
-    iron: '0%'
-  },
-  {
-    name: 'Lollipop',
-    calories: 392,
-    fat: 0.2,
-    carbs: 98,
-    protein: 0,
-    sodium: 38,
-    calcium: '0%',
-    iron: '2%'
-  },
-  {
-    name: 'Honeycomb',
-    calories: 408,
-    fat: 3.2,
-    carbs: 87,
-    protein: 6.5,
-    sodium: 562,
-    calcium: '0%',
-    iron: '45%'
-  },
-  {
-    name: 'Donut',
-    calories: 452,
-    fat: 25.0,
-    carbs: 51,
-    protein: 4.9,
-    sodium: 326,
-    calcium: '2%',
-    iron: '22%'
-  },
-  {
-    name: 'KitKat',
-    calories: 518,
-    fat: 26.0,
-    carbs: 65,
-    protein: 7,
-    sodium: 54,
-    calcium: '12%',
-    iron: '6%'
-  }
-]
 export default {
 name: "EditReport",
   data () {
     return {
-      data,
-      columns
+      model: [],
+      options: [
+        'Я', 'Н', 'Б', 'ОО', 'ГГ'
+      ]
     }
   }
 }
