4d5e3454cbbcf62db18d434010bea2aa319f0b3f
old-flex <kostyaniusgaming@gmail.com>
2021-04-09 06:41:57 +0700

    Новое создание репорта

--- src/pages/EditReport.vue
+++ src/pages/EditReport.vue
@@ -1,22 +1,21 @@
 <template>
-        <div class="flex items-center">
-          <q-markup-table separator="cell" flat bordered dense>
+          <q-markup-table  separator="cell" flat bordered dense>
             <tbody >
             <tr>
-              <td rowspan="2" class="text-center">Фамилия, инициалы, должность</td>
-              <td  rowspan="2" class="text-center">Табельный номер</td>
+              <td rowspan="2" class="text-center">Фамилия, <div>инициалы,</div> <div>должность</div></td>
+              <td  rowspan="2" class="text-center">Табельный <div>номер</div></td>
               <td  colspan="31" class="text-center">Отметки о явках и неявках</td>
             </tr>
             <tr>
-              <td v-for="index in 31" :key="index">
+              <td class="text-center" v-for="index in 31" :key="index">
                 {{ index }}
               </td>
             </tr>
             <tr>
               <td rowspan="2"  class="text-center">Витя Петров, <p>инженер</p></td>
               <td rowspan="2"  class="text-center">1123</td>
-              <td class="text-center" v-for="index in 31" :key="index">
-                <q-select dense v-model="model[index]" :options="options"/>
+              <td  v-for="index in 31" :key="index">
+                <q-select borderless dense v-model="model" :options="options"/>
               </td>
             </tr>
             <tr>
@@ -27,7 +26,6 @@
             </tbody>
 
           </q-markup-table>
-        </div>
 </template>
 
 <script>
@@ -35,7 +33,7 @@ export default {
 name: "EditReport",
   data () {
     return {
-      model: [],
+      model: ["Я"],
       options: [
         'Я', 'Н', 'Б', 'ОО', 'ГГ'
       ]
@@ -44,6 +42,43 @@ name: "EditReport",
 }
 </script>
 
-<style scoped>
-
+<style>
+.q-field__append.q-field__marginal.row.no-wrap.items-center.q-anchor--skip{
+  /*width: 0;*/
+  /*height: 0;*/
+  display: none;
+}
+.q-table tbody td {
+  font-size: 0.7rem;
+}
+.q-field__native.row.items-center {
+  font-size: 0.7rem;
+}
+.no-wrap {
+  flex-wrap: wrap;
+}
+.q-table--dense .q-table th, .q-table--dense .q-table td{
+  padding: 0;
+}
+.q-table--dense .q-table th:last-child, .q-table--dense .q-table td:last-child{
+  padding: 0;
+}
+.q-table--dense .q-table th:first-child, .q-table--dense .q-table td:first-child{
+  padding: 0;
+}
+.q-field__native.row.items-center{
+ display: flex;
+  justify-content: center;
+}
+th {
+  font-weight: 700;
+}
+.q-markup-table{
+  overflow: hidden;
+}
+@media (max-width:1300px) {
+  table{
+    width: 90%;
+  }
+}
 </style>
